f2b85787126758569b0dce61b0884559f9ff65db
martinovich89 <dsinemoi1mouton@gmail.com>
2022-03-12 19:59:54 +0100

    fixed various details in cpp01

---
+++ cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name)
+{
+    if (N < 0)
+        return (NULL);
+    Zombie *Horde = new Zombie[N];
+    for (int i = 0; i < N; i++)
+    {
+        Horde[i].set_name(name);
+    }
+    return (Horde);
+}
