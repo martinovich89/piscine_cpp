77c08cff30755f0124f78590314304cff2a689c5
mhenry <mhenry@student.42.fr>
2022-03-23 16:51:55 +0100

    split A B and C classes in 3 .hpp files in cpp06/ex02

---
+++ cpp06/ex02/A.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Base.hpp"
+#include <iostream>
+
+class Base;
+
+class A : public Base {};
--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -1,32 +1,10 @@
+#pragma once
+
 #include <iostream>
 
 class	Base
 {
-	public:
-
-		virtual ~Base();
-};
-
-class A : public Base
-{
-	public:
-
-		A();
-		~A();
-};
-
-class	B : public Base
-{
-	public:
-
-		B();
-		~B();
-};
-
-class	C : public Base
-{
-	public:
-
-		C();
-		~C();
+	public :
+	
+	virtual ~Base();
 };
